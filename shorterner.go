040ede9d6009306f138c8018bb3d207192230c10
nvtuongcs <nvtuongcs@gmail.com>
2019-04-18 05:38:59 +0700

    Save data in yaml file

--- shorterner.go
+++ shorterner.go
@@ -4,7 +4,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"io/ioutil"
+
+	"gopkg.in/yaml.v2"
 )
+
+type Mapper struct {
+	fullUrl string
+	shorternUrl string
+}
 
 func main() {
 	
@@ -20,21 +28,32 @@ func main() {
 	runCommand := flag.NewFlagSet("run", flag.ExitOnError)
 	port := addCommand.String("p", "", "add a new url")
 
+	// Read file
+	yamlFile, _ := ioutil.ReadFile("urls.yaml")
+	var data map[string]string
+	yaml.Unmarshal([]byte(yamlFile), &data)
+
 	switch os.Args[1] {
 	case "configure":
 		addCommand.Parse(os.Args[2:])
-		fmt.Println("%v", *shortUrl)
-		fmt.Println("%v", *fullUrl)
+		data[*shortUrl] = *fullUrl
+		text, _ := yaml.Marshal(data)
+		ioutil.WriteFile("urls.yaml", []byte(text), 0644)
 	case "run":
 		runCommand.Parse(os.Args[2:])
 		fmt.Println("%v", *port)
 	default:
 		flag.Parse()
 		if *listFlag {
-			fmt.Println("List of url")
+			for key, value := range data {
+				fmt.Println("Key: ", key, "value: ", value)
+			}
 		}
 		if *deleteFlag != "" {
 			fmt.Println("Delete command")
+			delete(data, *deleteFlag)
+			text, _ := yaml.Marshal(data)
+			ioutil.WriteFile("urls.yaml", []byte(text), 0644)
 		}
 	}
 }
